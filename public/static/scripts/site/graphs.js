f8c1fd80a4b7f997645469af91ba4f9a6ef19ec4
Irakli Mchedlishvili <ir.mchedlishvili@gmail.com>
2016-09-11 22:08:47 +0400

    [graphs][m]Seperate graphs added for each risk

--- public/static/scripts/site/graphs.js
+++ public/static/scripts/site/graphs.js
@@ -1,83 +1,58 @@
 var data = graphData
-var risks = [];
-data.forEach(function(risk){
-  if (!risks.includes(risk.risk)){
-    risks.push(risk.risk);
-  }
-});
-risks.push('all');
 
-var spec = {
-  "parameters": [
-    {
-        "signal": "Risks", "type": "select", "value": "all",
-        "options": risks
-    }
-  ],
-  "actions": {source: false, editor: false},
-  "spec":{
-    "width": 1000,
-    "height": 400,
-    "data": [
-      {
-        "name": "risks",
-        "values": data,
-        "format": {"parse": {"count": "number", "time":"date"}},
-        "transform": [
-            {
-                "type": "filter",
-                "test": "(Risks === 'all' || datum.risk === Risks)"
-            }
-        ]
-      }
-    ],
-    "scales": [
-      {
-        "name": "x",
-        "type": "time",
-        "range": "width",
-        "domain": {"data": "risks", "field": "time"}
-      },
-      {
-        "name": "y",
-        "type": "linear",
-        "range": "height",
-        "nice": true,
-        "domain": {"data": "risks", "field": "count"}
-      },
-      {
-        "name": "color", 
-        "type": "ordinal", 
-        "domain": {"data": "risks", "field": "risk"},
-        "range": "category10"
-      }
-    ],
-    "axes": [
-      {"type": "x", "scale": "x", "tickSizeEnd": 0},
-      {"type": "y", "scale": "y"}
-    ],
-    "marks": [
-      {
-        "type": "group",
-        "from": {
-          "data": "risks",
-          "transform": [{"type": "facet", "groupby": ["risk"]}]
+var risks = [
+  {risk: 'spam', color: 'blue'},
+  {risk:'opendns', color: 'black'},
+  {risk:'openntp', color:'red'},
+  {risk: 'openssdp', color: 'green'}
+  ];
+risks.forEach(function(risk){
+  var spec = {
+    "actions": false,
+    "spec":{
+      "width": 1080,
+      "height": 100,
+      "data": [
+        {
+          "name": "risks",
+          "values": data,
+          "format": {"parse": {"month":"date"}}
+        }
+      ],
+      "scales": [
+        {
+          "name": "x",
+          "type": "time",
+          "range": "width",
+          "domain": {"data": "risks", "field": "month"}
         },
-        "marks": [
-          {
-            "type": "line",
-            "properties": {
-              "enter": {
-                "x": {"scale": "x", "field": "time"},
-                "y": {"scale": "y", "field": "count"},
-                "stroke": {"scale": "color", "field": "risk"},
-                "strokeWidth": {"value": 2}
-              }
+        {
+          "name": "y",
+          "type": "linear",
+          "range": "height",
+          "nice": true,
+          "domain": {"data": "risks", "field": risk.risk}
+        }
+      ],
+      "axes": [
+        {"type": "x", "scale": "x", "ticks": 5, "grid": true},
+        {"type": "y", "scale": "y", "ticks": 3,"grid": true}
+      ],
+      "marks": [
+        {
+          "type": "line",
+          "from": {"data": "risks"},
+          "properties": {
+            "enter": {
+              "x": {"scale": "x", "field": "month"},
+              "y": {"scale": "y", "field": risk.risk},
+              "stroke": {"value": risk.color},
+              "strokeWidth": {"value": 4}
             }
           }
-        ]
-      }
-    ]
-  }
-};
-vg.embed('#vis', spec, function(error, result) {});
+        }
+      ]
+    }
+  };
+  vg.embed('#vis' + risk.risk, spec, function(error, result) {});
+});
