ce5d831028b1bf2fa6e8cd1552be545a3296173a
zelima <ir.mchedlishvili@gmail.com>
2016-09-21 19:16:20 +0400

    [scripts][s] Accepts tablename as second argument

--- logic.js
+++ logic.js
@@ -8,17 +8,18 @@ exports.getSingleEntry= function(data, matchWith, matchTo){
   return result;
 };
 
-exports.getEntriesFromDatabase = function(sequelize, options){
+exports.getEntriesFromDatabase = function(sequelize, table, options){
   var placeLogic = "1=1";
   var riskLogic = "1=1";
   var asnLogic = "1=1";
   var timeLogic = "1=1";
+
   if (options){
     if (options.place) placeLogic = "country = '" + options.place.toUpperCase() + "'";
     if (options.risk) riskLogic = "risk = '" + options.risk + "'";
     if (options.asn) asnLogic = "asn = '" + options.asn + "'";
     if (options.date) timeLogic = "date = '" + options.date + "'";
   }
-  var logic = "SELECT * FROM entries WHERE "+placeLogic+" AND "+riskLogic+" AND "+asnLogic+" AND "+timeLogic;
+  var logic = "SELECT * FROM "+table+" WHERE "+placeLogic+" AND "+riskLogic+" AND "+asnLogic+" AND "+timeLogic;
   return sequelize.query(logic);
 };
