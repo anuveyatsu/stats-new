1a595e9eb4fc89ee3084d258c3b772a5045a90be
zelima <ir.mchedlishvili@gmail.com>
2016-09-24 11:09:29 +0400

    [tests][s]: ASN number automated

--- tests/database_test.js
+++ tests/database_test.js
@@ -2,20 +2,21 @@ var Sequelize = require('sequelize');
 var dbConfig = require('../config').db;
 var logic = require('../logic');
 var assert = require('assert');
-var table = 'fixtures'
+var table = 'entries'
 
 sequelize = new Sequelize(
   dbConfig.database,
   dbConfig.username,
   dbConfig.password,
   dbConfig);
+var asn;
 
 describe('Database connection', function() {
   it('successfully connected', function(done) {   
     sequelize
       .authenticate()
       .then(function(err) { 
-        console.log('Unable to connect to the database:', err);
+      	if (err) console.log('Unable to connect to the database:', err);
         done();
     });
   }); 
@@ -24,7 +25,7 @@ describe('Database connection', function() {
 describe('Database Functions', function(){
   it('Works with single no options', function(done) {
     logic.getEntriesFromDatabase(sequelize, table).then(function(results){
-      assert.equal(results[0].length, 45000);
+      assert.equal(results[0].length, 56250);
       done();
     });
   });
@@ -49,21 +50,23 @@ describe('Database Functions', function(){
       assert.equal(results[0].length, 50);
       assert.equal(rand.risk, 2);
       assert.equal(rand.country, 'GB');
+      asn = rand.asn
       done();
     });
   });
   it('Works with triple filter', function(done) {
+    console.log(asn)
     options = {
       risk: 2,
       place: 'gb',
-      asn: 3214410
+      asn: asn
     };
     logic.getEntriesFromDatabase(sequelize, table, options).then(function(results){
       var rand = results[0][Math.floor(Math.random() * results[0].length)];
       assert.equal(results[0].length, 5);
       assert.equal(rand.risk, 2);
       assert.equal(rand.country, 'GB');
-      assert.equal(rand.asn, 3214410);
+      assert.equal(rand.asn, asn);
       done();
     });
   });
@@ -71,7 +74,7 @@ describe('Database Functions', function(){
     options = {
       risk: 4,
       place: 'gb',
-      asn: 3214410,
+      asn: asn,
       date: '2016-05-01'
     };
     logic.getEntriesFromDatabase(sequelize, table, options).then(function(results){
@@ -80,7 +83,7 @@ describe('Database Functions', function(){
       assert.equal(results[0].length, 1);
       assert.equal(rand.risk, 4);
       assert.equal(rand.country, 'GB');
-      assert.equal(rand.asn, 3214410);
+      assert.equal(rand.asn, asn);
       // todo: fix timezone problem
       //assert.equal(rand.month, time);
       done();
