eb2e76bef1bb831ea6837c042d515867adecd11e
zelima <ir.mchedlishvili@gmail.com>
2016-09-28 19:59:44 +0400

    [tests][s][closes #3]: database_tests.js and tests.js merged in logic.js

--- tests/logic.js
+++ tests/logic.js
@@ -3,7 +3,8 @@ var dbConfig = require('../config').db;
 var logic = require('../logic');
 var assert = require('assert');
 var request = require('supertest');
-var table = 'count  '
+
+var table = 'count';
 var app = require('../app.js').app;
 
 sequelize = new Sequelize(
@@ -179,3 +180,65 @@ describe('API', function(){
   });  
 });
 
+describe('Content', function(){
+  it('home page', function(done){
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function(err, res) {
+        checkContent(res, 'Tracking the state of global cyber health.');
+        done();
+      });
+  });
+  
+  it('place page', function(done){
+    request(app)
+      .get('/place')
+      .expect(200)
+      .end(function(err, res) {
+        checkContent(res, 'Place overview');
+        done();
+      });
+  });
+  
+  it('place/{id} page', function(done){
+    request(app)
+      .get('/place/united-kingdom')
+      .expect(200)
+      .end(function(err, res) {
+        checkContent(res, 'united-kingdom');
+        checkContent(res, 'Places');
+        done();
+      });
+  });
+  
+  it('risk page', function(done){
+    request(app)
+      .get('/risk')
+      .expect(200)
+      .end(function(err, res) {
+        checkContent(res, 'Overview of Risks');
+        checkContent(res, 'Open NTP'); 
+        done();
+      });
+  });
+  
+  it('risk/{id} page', function(done){
+    request(app)
+      .get('/risk/openntp/')
+      .expect(200)
+      .end(function(err, res) {
+        checkContent(res, 1);
+        checkContent(res, 'Devices'); 
+        done();
+      });
+  });
+});
+
+function checkContent(res, expected) {
+  var found = res.text.match(expected);
+  if (!found) {
+    console.log(res.text);
+    assert(false, '<<' + expected + '>> not found in page');
+  }
+}
