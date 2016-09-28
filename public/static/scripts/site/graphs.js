fc9048b7866b3c3d4161b7d5f4813379eba6e6dd
zelima <ir.mchedlishvili@gmail.com>
2016-09-28 19:47:02 +0400

    [graphs][s][#13]: Graph shows count on hover

--- public/static/scripts/site/graphs.js
+++ public/static/scripts/site/graphs.js
@@ -2,16 +2,49 @@ var data = graphData
 var risks = graphRisks
 
 risks.forEach(function(risk){
+	var template = "Count: {{parent."+String(risk.risk_id)+"}}";
 	var spec = {
     "actions": false,
     "spec":{
       "width": 1080,
       "height": 100,
+	  "signals": [
+        {
+          "name": "mouseDate",
+          "streams": [
+            {
+              "type": "mousemove",
+              "expr": "eventX()",
+              "scale": {"name": "x","invert": true}
+            }
+          ]
+        },
+        {
+          "name": "mouseCount",
+          "streams": [
+            {
+              "type": "mousemove",
+              "expr": "eventY()",
+              "scale": {"name": "y","invert": true}
+            }
+          ]
+        }
+      ],
       "data": [
         {
           "name": "risks",
           "values": data,
           "format": {"parse": {"month":"date"}}
+        },
+		{
+          "name": "count",
+          "source": "risks",
+          "transform": [
+            {
+              "type": "filter",
+              "test": "year(datum.month) == year(mouseDate) && month(datum.month) == month(mouseDate) && date(datum.month) == date(mouseDate)"
+            }
+          ]
         }
       ],
       "scales": [
@@ -45,7 +78,50 @@ risks.forEach(function(risk){
               "strokeWidth": {"value": 2}
             }
           }
-        }
+        },
+		{
+          "type": "line",
+          "from": {"data": "risks"},
+          "properties": {
+            "enter": {
+              "x": {"scale": "x", "field": "month"},
+              "y": {"scale": "y", "field": String(risk.risk_id)},
+              "stroke": {"value": "green"},
+              "strokeWidth": {"value": 2}
+            }
+          }
+        },
+		{
+          "type": "group",
+          "from": {"data": "count"},
+          "properties": {
+            "update": {
+              "x": {"scale": "x", "signal": "mouseDate", "offset": 5},
+              "y": {"scale": "y", "signal": "mouseCount", "offset": -10},
+              "width": {"value": 120},
+              "height": {"value": 30},
+              "fill": {"value": "#edf1f7"},
+              "fillOpacity": {"value": 0.85},
+              "stroke": {"value": "#aaa"},
+              "strokeWidth": {"value": 0.5}
+            }
+          },
+    
+          "marks": [
+            {
+              "type": "text",
+              "properties": {
+                "update": {
+                  "x": {"value": 6},
+                  "y": {"value": 19},
+                  "text": {"template": template},
+                  "fill": {"value": "black"},
+                  "align": {"value": "left"}
+                }
+              }
+            }
+          ]
+		}  
       ]
     }
   };
