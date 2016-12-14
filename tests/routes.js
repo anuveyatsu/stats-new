65616334a7fce02d742f5235219c9fd91da8c42e
zelima <ir.mchedlishvili@gmail.com>
2016-12-14 20:03:55 +0400

    [tests][m]: Tests checking statuses for all existing pages

--- tests/routes.js
+++ tests/routes.js
@@ -9,26 +9,28 @@ describe('Routes', function(){
       .get('/')
       .expect(200)
       .end(function(err, res) {
-        checkContent(res, 'Tracking the state of global cyber health.');
+        assert.equal(res.statusCode, 200);
         done();
-      });
+    });
   });
   
-  it('place page', function(done){
+  it('country/ page', function(done){
     request(app)
       .get('/country')
       .expect(200)
       .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
         checkContent(res, 'Country overview');
         done();
-      });
+    });
   });
   
-  it('place/{id} page', function(done){
+  it('country/{country_id} page', function(done){
     request(app)
       .get('/country/united-kingdom')
       .expect(200)
       .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
         checkContent(res, 'united-kingdom');
         checkContent(res, 'Countries');
       });
@@ -36,32 +38,132 @@ describe('Routes', function(){
       .get('/country/United-Kingdom')
       .expect(200)
       .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
         checkContent(res, 'united-kingdom');
         checkContent(res, 'Countries');
         done();
-      });
+    });
   });
   
-  it('risk page', function(done){
+  it('risk/ page', function(done){
     request(app)
       .get('/risk')
       .expect(200)
       .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
         checkContent(res, 'Overview of Risks');
         checkContent(res, 'Open NTP'); 
         done();
-      });
+    });
   });
   
-  it('risk/{id} page', function(done){
+  it('risk/risk_id page', function(done){
     request(app)
       .get('/risk/openntp/')
       .expect(200)
       .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
         checkContent(res, 1);
         checkContent(res, 'Devices'); 
         done();
-      });
+    });
+  });
+  
+  it('country/{country_id}/{risk_id} page', function(done){
+    request(app)
+      .get('/country/united-kingdom/openntp/')
+      .expect(200)
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
+        checkContent(res, 'Open NTP');
+        checkContent(res, 'United Kingdom'); 
+        done();
+    });
+  });
+  
+  it('Country ASN page', function(done){
+    request(app)
+      .get('/country/united-kingdom/asn/44444')
+      .expect(200)
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
+        checkContent(res, 44444); 
+        done();
+    });
+  });
+  
+  it('country/{country_id}/asn redirected back to country/{country_id} page', function(done){
+    request(app)
+      .get('/country/united-kingdom/asn')
+      .expect(200)
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 302);
+        assert.equal(res.redirect, true);
+        assert.equal(res.header.location, '/country/united-kingdom');
+        done();
+    });
+  });
+  
+  it('Download And API Page', function(done){
+    request(app)
+      .get('/asn')
+      .expect(200)
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
+        done();
+    });
+  });
+  
+  it('Download And API Page', function(done){
+    request(app)
+      .get('/download')
+      .expect(200)
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
+        checkContent(res, 'Download'); 
+        done();
+    });
+  });
+  
+  it('About page', function(done){
+    request(app)
+      .get('/about')
+      .expect(200)
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
+        checkContent(res, 'About'); 
+        done();
+    });
+  });
+  
+  it('Map page', function(done){
+    request(app)
+      .get('/vis/map/embed')
+      .expect(200)
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
+        done();
+    });
+  });
+  
+  it('Date page', function(done){
+    request(app)
+      .get('/data/geo.json')
+      .expect(200)
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
+        done();
+    });
+  });
+  
+  it('Date page for geo.json', function(done){
+    request(app)
+      .get('/data/geo.json')
+      .expect(200)
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
+        done();
+    });
   });
 });
 
